cf2298302497365ad86ed244997d5541dab83429
Vince <42632244+bigfoot79@users.noreply.github.com>
2018-09-21 13:46:47 +1000

    initial example, simple matrix multiplication.

---
+++ num01.py
@@ -0,0 +1,19 @@
+
+# import declarations
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+# method/statement declarations
+
+    
+
+def main():
+
+    
+          
+if __name__ == '__main__':
+
+    main()
+    
